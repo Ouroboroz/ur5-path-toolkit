c486061fd361789a0451027939073e200999b9aa
Kevin Yang <hbyang2000@gmail.com>
2018-11-01 17:21:57 -0400

    set ik

--- ur5-tools/utils/ik_correction.py
+++ ur5-tools/utils/ik_correction.py
@@ -4,3 +4,11 @@ import bottom_limit_check, direction_check, mesh_intersect
 """
 Runner script to correct ik contraints to fix the issues brought up by the checks
 """
+
+def set_ik_contraints(bone, axis = 'y', min, max):
+	"""
+	Sets the ik_contraints for the input bone on the input axis. 
+
+	The axis will default to 'y' since that is the rotation axis for bones in the UR5
+	"""
+	
